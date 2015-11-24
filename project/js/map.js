5e885354813a417cd613bcfe86c2d530256ab829
froehr <f.roehr@uni-muenster.de>
2015-11-24 17:15:50 +0100

    New features like scalebar and layerswitcher

--- project/js/map.js
+++ project/js/map.js
@@ -15,20 +15,30 @@ var map = new L.Map('map', {
     scale : true
 });
 
+// Scalebar
+var Lscalebar = new L.control.scale({
+    position : 'bottomright',
+    metric : true,
+    imperial : false
+}).addTo(map)
+
 // insert open street map layer into map
 var osm_hot = new L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// insert open street map layer into map
 var osm_mq = new L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
     attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     subdomains : ['otile1', 'otile2', 'otile3', 'otile4']
 });
 
+// insert open street map layer into map
 var osm_mapnik = new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
 
+// Create basemap switch
 var baseMaps = {
     "OSM Hot": osm_hot,
     "OSM MQ": osm_mq,
