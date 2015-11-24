f1afd161350008c162ce9d1705386a0ca08365f3
froehr <f.roehr@uni-muenster.de>
2015-11-24 17:16:04 +0100

    minor rearrangement

--- project/js/highcharts.js
+++ project/js/highcharts.js
@@ -1,41 +1,43 @@
-// Chart for gender comparison
-$(function () {
+// Chart for displaying migration
+function callHighcharts(data) {
     $('#highchartsData').highcharts({
-        chart: {
-            type: 'line'
-        },
-        title: {
-            text: 'Placeholder'
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Placeholder'
-            }
-        },
-
-        xAxis: {
-            min: 0,
+            chart: {
+                type: 'line'
+            },
             title: {
                 text: 'Placeholder'
             },
-            categories: ["2011", "2012", "2013"]
-        },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Placeholder'
+                }
+            },
 
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
+            xAxis: {
+                min: 0,
+                title: {
+                    text: 'Placeholder'
+                },
+                categories: ["2011", "2012", "2013"]
+            },
 
-        series: [{
-            name: 'Migration',
-            data: [115, 156, 10],
-            color: '#B20047'
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
 
-        }]
+            series: [{
+                name: 'Migration',
+                data: data,
+                color: '#B20047'
+
+            }]
     });
-});
+}
+
+callHighcharts([1,2,3]);
