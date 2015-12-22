0fe75e6620e8052fed23324fedd9887e601a0e28
froehr <f.roehr@uni-muenster.de>
2015-12-22 14:26:03 +0100

    Finished Documentation, new tooltip, ne variable for setting offset of y axis

--- project/js/highcharts.js
+++ project/js/highcharts.js
@@ -1,15 +1,15 @@
 //@function callHighcharts renders the data from the triple store to a chart using highcharts
 //@param array xAxis is an array, which stores the labels for the xAxis as string, int or float
 //@param array yAxis is an array, which stores the values for the xAxis as int or float
-//@param string xAxisTitel
-//@param string yAxisTitel
-//@param string chartTitel
-//@param string seriesName
-//@param string chartType
+//@param string xAxisTitel is the title of the x axis
+//@param string yAxisTitel is the title of the y axis
+//@param int yAxisMinValue is an offset for the y axis so that the axis is adjusted to the data
+//@param string chartTitel is the title for the complete chart
+//@param string seriesName is the name of the data series itself
+//@param string chartType makes it possible to set the chart type like bar, line, column etc.
 //@info xAxis.length() and yAxis.length must be equal
 //@return none
-function callHighcharts(xAxis, yAxis, xAxisTitel, yAxisTitel, chartTitel, seriesName, charType) {
-    console.log("finish documentation!")
+function callHighcharts(xAxis, yAxis, xAxisTitel, yAxisTitel, yAxisMinValue, chartTitel, seriesName, charType) {
     $('#highchartsData').highcharts({
             chart: {
                 type: charType,
@@ -18,18 +18,15 @@ function callHighcharts(xAxis, yAxis, xAxisTitel, yAxisTitel, chartTitel, series
                     }
                 }
             },
-
             title: {
                 text: chartTitel
             },
-
             yAxis: {
-                min: 0,
+                min: yAxisMinValue,
                 title: {
                     text: yAxisTitel
                 }
             },
-
             xAxis: {
                 min: 0,
                 title: {
@@ -37,23 +34,19 @@ function callHighcharts(xAxis, yAxis, xAxisTitel, yAxisTitel, chartTitel, series
                 },
                 categories: xAxis
             },
-
             tooltip: {
-                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
-                footerFormat: '</table>',
                 shared: true,
-                useHTML: true
+                useHTML: true,
+                headerFormat: '<b>{point.key}</b><table>',
+                pointFormat: '<tr><td style="color: {series.color}">{series.name}:</td></tr>' +
+                '<tr><td><b>{point.y} persons</b></td></tr>',
+                footerFormat: '</table>',
             },
-
             series: [{
                 name: seriesName,
                 data: yAxis,
                 color: '#B20047'
-
             }]
     });
 }
-
 callHighcharts([],[], "", "", "", "");
