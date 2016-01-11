ae03940bb2e326e9c48a9363c71a6ae2c3475bbd
joannakjw <joannakjw@gmail.com>
2016-01-11 15:44:28 +0100

    added help. will add more when the webpage makes more progress!

--- project/js/intro.js
+++ project/js/intro.js
@@ -5,14 +5,24 @@ function startIntro(){
         steps: [
             {
                 element: "#map",
-                intro: "This will be the first help step",
+                intro: "Feel free to browse around the City of Münster and click on each area that you are interested in.",
                 position: "right"
             },
             {
-                element: "#infoPanel",
-                intro: "This will be the second help step",
+                element: ".leaflet-control-layers",
+                intro: "Here, you can choose the background map. You can also see the data for all of Münster, its boroughs, or its districts.",
                 position: "left"
-            }
+            },            
+            {
+                element: "#infoPanel",
+                intro: "This provides you with the linked data information for each area that you click.",
+                position: "left"
+            },
+            {
+                element: ".dropdown",
+                intro: "Choose the information that you want to see in a chart.",
+                position: "left"
+            }        
         ]
     });
     intro.start();
